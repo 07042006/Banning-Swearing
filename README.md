746461465354ac8b86fff89f1ef1cf42cd53f46b
Bruno Fox <50891842+07042006@users.noreply.github.com>
2023-11-10 22:09:18 -0300

    Upload project
    Upload project base

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Banning-Swearing
 Automation in Python to ban (punish) for swearing.
+
+## Requiments
+pip install SpeechRecognition plyer
+pip install pyaudio
---
+++ main.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+from plyer import notification
+
+def enviar_notificacao(palavra):
+    mensagem = f"Palavra identificada: {palavra}"
+    notification.notify(
+        title="Alerta de Palavra",
+        message=mensagem,
+        app_name="Identificador de Palavras"
+    )
+
+def verificar_palavras(texto, palavras):
+    for palavra in palavras:
+        if palavra in texto:
+            return palavra
+    return None
+
+def main():
+    with open('arquivo.txt', 'r') as arquivo:
+        palavras_lista = [linha.strip() for linha in arquivo]
+
+    recognizer = sr.Recognizer()
+
+    while True:
+        try:
+            with sr.Microphone() as source:
+                print("Ouvindo...")
+                audio = recognizer.listen(source, timeout=10)
+
+            texto = recognizer.recognize_google(audio, language="pt-BR").lower()
+            print("Texto reconhecido:", texto)
+
+            palavra_identificada = verificar_palavras(texto, palavras_lista)
+
+            if palavra_identificada:
+                enviar_notificacao(palavra_identificada)
+
+        except sr.UnknownValueError:
+            print("Não foi possível entender o áudio")
+        except sr.RequestError as e:
+            print(f"Erro ao fazer a requisição ao serviço de reconhecimento de voz; {e}")
+
+if __name__ == "__main__":
+    main()
